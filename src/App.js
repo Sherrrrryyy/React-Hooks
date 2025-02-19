c119d38072e1f21d73f3ee0e2314eb762edd048b
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-02-19 12:39:21 +0500

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   // useState Hook
-
   // const [count, setCount] = useState(0);
 
   // const addNumber = () => {
@@ -20,8 +19,28 @@ function App() {
 
   // const nameChange = () =>{
   //   setName('Hasnain Ali')
-  
+
   // }
+
+
+  // // useEffect with dependancy
+  // useEffect(()=>{
+  //   console.log('useEffect Hook is running');
+  // }, [])
+
+  // // useEffect without dependancy
+  // useEffect(()=>{
+  //   console.log('useEffect Hook is running');
+  // })
+
+
+  const [data, setData] = useState([])
+
+  //useEffect with API
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products').then((response) => response.json()).then((json) => setData(json))
+  }, [])
+
 
   return (
     <>
@@ -30,12 +49,32 @@ function App() {
         <button onClick={addNumber}>Click me to increase the number</button>
         <button onClick={subNumber}>Click me to increase the number</button>
       </div> */}
-{/* 
+      {/* 
       <div>
         <h1>Enter your name</h1>
         <input type="text" value={name} onChange={nameChange} />
       </div> */}
 
+
+      <div style={{ justifyContent: 'center' }}>
+        <h1>PRODUCTS</h1>
+        <ul>
+          {data.map((item) => {
+            return (
+              <>
+                <li>{item.title}</li>
+                <li>{item.price}</li>
+                <li>{item.category}</li>
+                <li>{item.rating[1]}</li>
+
+              </>
+            )
+          })}
+        </ul>
+
+
+
+      </div>
 
     </>
   );
