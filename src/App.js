a3a8ba6d89ebc4c34aae7afcf611725bce3ebe70
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-02-19 12:22:39 +0500

    useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  // useState Hook
+
+  // const [count, setCount] = useState(0);
+
+  // const addNumber = () => {
+  //   setCount(prev => prev + 2)
+  // }
+
+  // const subNumber = () => {
+  //   setCount(prev => prev - 1)
+  // }
+
+  // const [name, setName] = useState('Hasnain');
+
+  // const nameChange = () =>{
+  //   setName('Hasnain Ali')
+  
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <div>
+        <h1>{count}</h1>
+        <button onClick={addNumber}>Click me to increase the number</button>
+        <button onClick={subNumber}>Click me to increase the number</button>
+      </div> */}
+{/* 
+      <div>
+        <h1>Enter your name</h1>
+        <input type="text" value={name} onChange={nameChange} />
+      </div> */}
+
+
+    </>
   );
 }
 
