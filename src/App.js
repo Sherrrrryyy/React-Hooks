99fab3e3b0b955b05fd3de53a3c605447cea5f12
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-05-06 08:48:50 +0500

    Comment out additional unused state and effect hooks in App component

--- src/App.js
+++ src/App.js
@@ -4,57 +4,71 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  // useState Hook
-  // const [count, setCount] = useState(0);
-  
-  // const addNumber = () => {
-  //     setCount(prev => prev + 2)
-  //   }
-    
-  //   const subNumber = () => {
-  //       setCount(prev => prev - 1)
-  //     }
-      
-      // const [name, setName] = useState('Hasnain');
-      
-      // const nameChange = () =>{
-        //   setName('Hasnain Ali')
-        
-        // }
-        
-        
-        // // useEffect with dependancy
-        // useEffect(()=>{
-          //   console.log('useEffect Hook is running');
-          // }, [])
+  // const [name, setName] = useState('Hasnain');
 
-          // // useEffect without dependancy
-          // useEffect(()=>{
+  // const nameChange = () =>{
+  //   setName('Hasnain Ali')
+
+  // }
+
+
+  // // useEffect with dependancy
+  // useEffect(()=>{
+  //   console.log('useEffect Hook is running');
+  // }, [])
+
+  // // useEffect without dependancy
+  // useEffect(()=>{
   //   console.log('useEffect Hook is running');
   // })
-  
-  
+
+
   // const [data, setData] = useState([])
-  
+
   //useEffect with API
   // useEffect(() => {
-    //   fetch('https://fakestoreapi.com/products').then((response) => response.json()).then((json) => setData(json))
-    // }, [setData])
+  //   fetch('https://fakestoreapi.com/products').then((response) => response.json()).then((json) => setData(json))
+  // }, [setData])
+
+  // useState Hook Example 1 
+  // const [count, setCount] = useState(0)
+
+  // useState Hook Example 2
+  // const [name, setName] = useState('Enter Your Name')
+
+  // useState Hook Example 3
+  const [data, setData] = useState([]);
+  const [name, setName] = useState('');
+
+  const handleInput = (e) => {
+    setName(e.target.value)
+  }
+
+
+  const saveUserData = () => {
+    setData((prevData) => [...prevData, name]);
+    setName('')
+ console.log(data);
     
-    const [count, setCount] = useState(0)
-    
-    return (
-      <>
-      <div>
+  }
+
+
+  return (
+    <>
+
+      {/* Example 1 */}
+
+      {/* <div>
         <h1>Count: {count}</h1>
         <button onClick={()=> setCount(count + 1)}>Click me to increase the number</button>
         <button onClick={()=> setCount(count -1)}>Click me to increase the number</button>
-      </div>
-      {/* 
+      </div> */}
+
       <div>
         <h1>Enter your name</h1>
-        <input type="text" value={name} onChange={nameChange} />
-      </div> */}
+        <input type="text" value={name} onChange={handleInput} />
+        <button onClick={saveUserData}>Submit</button>
+      </div>
 
 
       {/* useEffect */}
